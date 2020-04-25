250e4f877f4a9f15d641f9ee41231fbecd961c82
sdfsfd <sdfd@sdfsd.ff>
2020-04-25 14:08:00 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Saplin.TimeSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var series = new List<double>();
+            var  awaiter = new ManualResetEventSlim(false);
+            var timer = new System.Timers.Timer();
+            var rand = new Random();
+            int n = -60;
+
+            var ascii = new AsciiTimeSeries();
+            ascii.HeigthLines = 15;
+            ascii.WidthCharacters = 50;
+            ascii.EmptyChar = '`';
+            ascii.AbovePointChar = '`';
+            ascii.Series = series;
+
+            timer.Elapsed += (s, e) =>
+            {
+                series.Add(5 * Math.Cos(Math.PI * n / 45) * (1 + 0.2 * rand.NextDouble()) + 1.85 * rand.NextDouble());
+                n++;
+                Console.WriteLine(ascii.RenderToString());
+                Console.CursorTop -= ascii.HeigthLines;
+            };
+
+            timer.Interval = 200;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+            Console.WriteLine("Press any key to quit");
+            awaiter.Wait();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# AsciiTimeSeries
+ C# class that renders a numerical collection (double) to a string which displayed with monospace font will be a time series chart 
